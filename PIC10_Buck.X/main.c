99d9aea1242b3e8ae552f6be61949a5edb2bd969
pyrohaz <harris.shallcross@gmail.com>
2016-03-06 23:26:51 +0000

    Added comments

--- PIC10_Buck.X/main.c
+++ PIC10_Buck.X/main.c
@@ -20,22 +20,33 @@
 volatile uint8_t aConv = 0, dutyCycle;
 volatile uint8_t tick = 0;
 
+//Delay for 'd' ticks. Maximum delay of 255 (~4ms)
 void delay(uint8_t d){
     tick = 0;
     while(tick<d);
 }
 
-void interrupt isr(void){    
+
+void interrupt isr(void){ 
+    //Timer 2 overflow ISR
     if(PIR1bits.TMR2IF){
         PIR1bits.TMR2IF = 0;
-        //Start ADC Conversion
-        ADCONbits.GO_nDONE = 1;
-
+        
+        //Get ADC value from previous conversion
         aConv = ADRES;
+        
+        //Execute controller! If the read analog value is lower than the 
+        //reference and the duty cycle hasn't saturated, increment the duty
+        //cycle by 1. If the read analog value is more than the reference
+        //and the duty cycle is more than 0, decrement the duty cycle.
+        
         if(aConv<DREF && dutyCycle < 64) dutyCycle++;
         else if(dutyCycle>0) dutyCycle--;
         
+        //Write the duty cycle to the PWM register
         PWM1DCH = dutyCycle;
+        
+        //Increment the tick counter
         tick++;
         
         //Start conversion for next cycle
@@ -65,16 +76,20 @@ void main(void)
     T2CON = 0x04;
     PWM1CON = 0xF0;
     
+    //Wait for timer overflow
     while(!PIR1bits.TMR2IF);
     TRISAbits.TRISA0 = 0;
-    PWM1DCH = 10;
     
+    //Initialize RA1 for step response sink
     TRISAbits.TRISA1 = 0;
 
     while(1)
     {
+        //Sink current
         PORTAbits.RA1 = 0;
         delay(100);
+        
+        //Stop sinking
         PORTAbits.RA1 = 1;
         delay(100);
         delay(200);
